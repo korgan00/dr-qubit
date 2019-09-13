94454a6d14bb56fe7835ef0dae6710e541716a30
y4izus <yaiza.garcia.mm@gmail.com>
2019-09-13 22:46:00 +0200

    Generates initial board with classical bits

--- Doctor qubit/Assets/Scripts/InitialBoardState.cs
+++ Doctor qubit/Assets/Scripts/InitialBoardState.cs
@@ -1,9 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class InitialBoardState : ScriptableObject {
 
     public Vector2Int boardSize;
+    //public int[,] elements => new int[boardSize[0], boardSize[1]];
 
+    [SerializeField]
+    public int numClassiBits = 2;
+
+    [SerializeField]
+    public int maxToShowBits = 5;
+
+    public ClassicBit[,] getInitialBoard(Vector2Int boardSize)
+    {
+        ClassicBit [,] board = new ClassicBit[boardSize[0], boardSize[1]];
+
+        for (int i = 0; i < numClassiBits; i++)
+        {
+
+            Vector2Int randomCoord = calculateRandomCoord();
+
+            while(board[randomCoord.x, randomCoord.y] || randomCoord.y > maxToShowBits)
+            {
+                randomCoord = calculateRandomCoord();
+            }
+
+            board[randomCoord.x, randomCoord.y] = new ClassicBit();
+
+        }
+
+        return board;
+    }
+
+    private Vector2Int calculateRandomCoord()
+    {
+        Random rnd = new Random();
+        int randomX = rnd.Next(0, boardSize[0]);
+        int randomY = rnd.Next(0, boardSize[1]);
+
+        return new Vector2Int(randomX, randomY);
+    }
 }
