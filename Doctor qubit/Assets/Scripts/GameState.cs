b3bd6f046848acc8a884a8c630ef87fd644e5abe
Korgan <korgan00@gmail.com>
2019-09-13 16:48:46 +0200

    Stage and qubit movements

--- Doctor qubit/Assets/Scripts/GameState.cs
+++ Doctor qubit/Assets/Scripts/GameState.cs
@@ -2,17 +2,93 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameState : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+public class GameState : MonoBehaviour {
+
+    private static GameState _instance;
+
+    public GameState instance {
+        get { return _instance; }
     }
 
+    [SerializeField]
+    private Vector2Int _boardSize;
+    [SerializeField]
+    public float speed = 1.0f;
+    [SerializeField]
+    public float stepsPerBlock = 1.0f;
+    [SerializeField]
+    private QubitMovement _instanciableQubit;
+    [SerializeField]
+    private Transform _spawnPoint;
+
+    private QubitMovement _currentQubit;
+    private GameObject[,] _board;
+    private float _currentMovementColdDown;
+    private float movementColdDown {
+        get { return 1f / stepsPerBlock; }
+    }
+
+    private bool finished;
+
+    private void Start() {
+        _currentMovementColdDown = movementColdDown;
+        SpawnNextQubit();
+        _board = new GameObject[8,16];
+    }
+
+
     // Update is called once per frame
-    void Update()
-    {
+    private void Update() {
+        _currentMovementColdDown -= Time.deltaTime * speed;
+
         
+        if (Input.GetKeyDown(KeyCode.A)) {
+            MoveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            MoveRight();
+        }
+        //if (Input.GetKeyDown)
+
+        
+
+        while (_currentMovementColdDown < 0) {
+            _currentMovementColdDown += movementColdDown;
+            _currentQubit.MoveVertical(1f / stepsPerBlock);
+            Vector2Int boardPosition = new Vector2Int(Mathf.FloorToInt(_currentQubit.position.x), Mathf.FloorToInt(_currentQubit.position.y));
+            if (boardPosition.y < 0 || _board[boardPosition.x, boardPosition.y] != null) {
+                _currentQubit.MoveVertical(-1f / stepsPerBlock);
+                Vector2Int newBoardPosition = new Vector2Int(Mathf.FloorToInt(_currentQubit.position.x), Mathf.FloorToInt(_currentQubit.position.y));
+                _board[newBoardPosition.x, newBoardPosition.y] = _currentQubit.gameObject;
+                _currentQubit.StopMoving();
+                EvaluateGameState();
+                if (!finished) {
+                    SpawnNextQubit();
+                }
+            }
+        }
+    }
+
+    public void MoveRight() {
+        if (_currentQubit.position.x + 1 < _boardSize.x) {
+            _currentQubit.MoveHorizontal(1f);
+        }
+    }
+
+    public void MoveLeft() {
+        if (_currentQubit.position.x - 1 >= 0) {
+            _currentQubit.MoveHorizontal(-1f);
+        }
+    }
+
+    private void SpawnNextQubit() {
+        _currentQubit = Instantiate(_instanciableQubit, _spawnPoint.position, _spawnPoint.rotation);
+        _currentQubit.position.y = _boardSize.y;
+        _currentQubit.MoveHorizontal(_boardSize.x / 2);
+    }
+    
+    private void EvaluateGameState() {
+
     }
 }
---
+++ Doctor qubit/Assets/Scripts/QubitMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QubitMovement : MonoBehaviour {
+
+    public Vector2 position;
+
+    public void MoveVertical(float amount) {
+        position.y -= amount;
+        transform.position = transform.position - (Vector3.up * amount);
+    }
+
+    public void MoveHorizontal(float amount) {
+        position.x += amount;
+        transform.position = transform.position + (Vector3.right * amount);
+    }
+
+    public void StopMoving() {
+        Destroy(this);
+    }
+}
