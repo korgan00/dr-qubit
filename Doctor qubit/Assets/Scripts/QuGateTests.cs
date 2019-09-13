b6e22819b48347a3fe9c5082d242730af6f9a7b3
y4izus <yaiza.garcia.mm@gmail.com>
2019-09-13 17:06:54 +0200

    Adds test cases

--- Doctor qubit/Assets/Scripts/QuGateTests.cs
+++ Doctor qubit/Assets/Scripts/QuGateTests.cs
@@ -8,30 +8,33 @@ public class QuGateTests : MonoBehaviour
     {
         Vector3 vector0 = new Vector3(0, 0, 1);
         Vector3 vector1 = new Vector3(0, 0, -1);
+        Vector3 vectorSuperpositionPlus = new Vector3(1, 0, 0);
+        Vector3 vectorSuperpositionMinus = new Vector3(-1, 0, 0);
 
-        Debug.Log("===Test Gate X===");
         Vector3 rotationVectorForX = new Vector3(180, 0, 0);
         Quaternion rotationForX = Quaternion.Euler(rotationVectorForX);
-        Debug.Log($"X * |0>: {rotationForX * vector0}");
-        Debug.Log("Expected State: 0 0 -1");
-        Debug.Log($"X * |1>: {rotationForX * vector1}");
-        Debug.Log("Expected State: 0 0 1");
+        Debug.Log($"X * |0>: {rotationForX * vector0} == 0 0 -1");
+        Debug.Log($"X * |1>: {rotationForX * vector1} == 0 0 1");
+        Debug.Log($"X * |0> + |1>: {rotationForX * vectorSuperpositionPlus} == 1 0 0");
+        Debug.Log($"X * |0> - |1>: {rotationForX * vectorSuperpositionMinus} == -1 0 0");
 
-        Debug.Log("===Test Gate Z===");
         Vector3 rotationVectorForZ = new Vector3(0, 0, 180);
         Quaternion rotationForZ = Quaternion.Euler(rotationVectorForZ);
-        Debug.Log($"Z * |0>: {rotationForZ * vector0}");
-        Debug.Log("Expected State: 0 0 1");
-        Debug.Log($"Z * |1>: {rotationForZ * vector1}");
-        Debug.Log("Expected State: 0 0 -1");
+        Debug.Log($"Z * |0>: {rotationForZ * vector0} == 0 0 1");
+        Debug.Log($"Z * |1>: {rotationForZ * vector1} == 0 0 -1");
+        Debug.Log($"Z * |0> + |1>: {rotationForZ * vectorSuperpositionPlus} == -1 0 0");
+        Debug.Log($"Z * |0> - |1>: {rotationForZ * vectorSuperpositionMinus} == 1 0 0");
 
-        Debug.Log("===Test Gate H===");
         Vector3 rotationVectorForH = new Vector3(0, 90, 180);
         Quaternion rotationForH = Quaternion.Euler(rotationVectorForH);
-        Debug.Log($"H * |0>: {rotationForH * vector0}");
-        Debug.Log("Expected State: 1 0 0");
-        Debug.Log($"H * |1>: {rotationForH * vector1}");
-        Debug.Log("Expected State: -1 0 0");
-    }
+        Debug.Log($"H * |0>: {rotationForH * vector0} == 1 0 0");
+        Debug.Log($"H * |1>: {rotationForH * vector1} == -1 0 0");
+        Debug.Log($"H * |0> + |1>: {rotationForH * vectorSuperpositionPlus} == 0 0 1");
+        Debug.Log($"H * |0> - |1>: {rotationForH * vectorSuperpositionMinus} == 0 0 -1");
 
+
+        Debug.Log($"H * X * |0>: {rotationForH * rotationForX * vector0} == -1 0 0");
+        Debug.Log($"X * H * |0>: {rotationForX * rotationForH * vector0} == 1 0 0");
+
+    }
 }
